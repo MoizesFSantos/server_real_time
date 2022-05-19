ed98d055b9eb6532594cc54ef1977506483aea0e
MoizesFSantos <moizesferreira66@hotmail.com>
2022-05-19 10:56:36 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 8899;
+
+app.listen(PORT, ()=> console.log(`Server is running at ${PORT}`));
